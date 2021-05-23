7df45fa43d22a982e132af59d1b7482fde3dc6f2
sahil-75 <sahil.kalyani75@gmail.com>
2021-05-23 21:41:21 +0530

    Sidebar Menu changes

--- src/components/SideBar/SideBar.js
+++ src/components/SideBar/SideBar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { navigate } from '@reach/router';
 import { selectUser } from '../../features/user/user.selector';
@@ -12,17 +12,21 @@ import {
 	FaGamepad,
 	FaSignOutAlt,
 	FaBriefcase,
+	FaUserClock
 } from 'react-icons/fa';
 import { userActions } from '../../features/user/user.slice';
 
 const SideBar = (props) => {
 	const dispatch = useDispatch();
 	const user = useSelector(selectUser) ?? {};
+	const [isProfileMenuVisible, toggleProfileMenu] = useState(false);
+
 	return (
 		<div className='py-3 flex-col h-full bg-blue-500 rounded-md flex items-center justify-between shadow-2xl text-gray-50'>
 			<div className='flex-col'>
 				<div className='my-3 px-2 mb-6 flex items-center justify-center'>
-					<div className='rounded-full border-cyan-300 ring-2 ring-white'>
+					<div className='rounded-full border-cyan-300 ring-2 ring-white cursor-pointer'
+						onClick={() => toggleProfileMenu(!isProfileMenuVisible)}>
 						{user.image && (
 							<img alt='User' src={user.image} className='w-10' />
 						)}
@@ -51,19 +55,11 @@ const SideBar = (props) => {
 				<button className='h-12 flex items-center text-xl my-3 p-4 text-gray-300'>
 					<FaCalendarAlt />
 				</button>
-				{user.role === 'admin' && (
-					<button
-						className='h-12 flex items-center text-xl my-3 cursor-pointer p-4'
-						onClick={() => navigate('/signup')}
-					>
-						<FaUserPlus size={24} />
-					</button>
-				)}
 			</div>
 			<div className='flex-col flex items-center'>
-				{props.inBreak && (
+				{true && (
 					<button
-						className='h-12 flex items-center text-xl my-6 cursor-pointer p-2'
+						className='h-12 flex items-center text-xl my-4 cursor-pointer p-2'
 						onClick={() =>
 							props.setBreakMode &&
 							props.setBreakMode(!props.breakMode)
@@ -88,8 +84,30 @@ const SideBar = (props) => {
 						breakMode={props.breakMode}
 					/>
 				</button>
+			</div>
+			<div
+				className='absolute left-6 p-2 text-xl rounded-xl top-20 mt-1
+				bg-white shadow-lg flex flex-col'
+				style={{ visibility: isProfileMenuVisible ? 'visible' : 'hidden' }}
+			>
+				{user.role === 'admin' && (
+					<button
+						className='flex flex-row items-center p-2 text-xl cursor-pointer text-blue-500'
+						onClick={() => navigate('/signup')}
+					>
+						<FaUserPlus size={24} />
+						<h4 className='text-gray-500 ml-3 text-base cursor-pointer'>Add User</h4>
+					</button>
+				)}
 				<button
-					className='h-12 flex items-center text-xl p-4 cursor-pointer'
+					className='flex flex-row items-center p-2 text-xl cursor-pointer text-blue-500'
+					onClick={() => { alert('Delegatee Set') }}
+				>
+					<FaUserClock size={24} />
+					<h4 className='text-gray-500 ml-3 text-base cursor-pointer'>Set Delegatee</h4>
+				</button>
+				<button
+					className='flex flex-row items-center p-2 text-xl cursor-pointer text-blue-500'
 					onClick={() => {
 						const sure = window.confirm(
 							'Are you sure you want to logout?',
@@ -100,7 +118,8 @@ const SideBar = (props) => {
 						}
 					}}
 				>
-					<FaSignOutAlt />
+					<FaSignOutAlt size={24} />
+					<h4 className='text-gray-500 ml-3 text-base cursor-pointer'>Log Out</h4>
 				</button>
 			</div>
 		</div>
