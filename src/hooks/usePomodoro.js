a1ce2140efee5685f05a1c2119c103edb075d44d
Ankit Kumar Pandit <ankitp@geekyants.com>
2021-05-24 11:06:16 +0530

    Pomodoro hours updated

--- src/hooks/usePomodoro.js
+++ src/hooks/usePomodoro.js
@@ -99,8 +99,8 @@ const isCurrentInRange = (start, end) => {
 const getPomodoroArray = ({ workTime, breakTime, lunchTime, pomodoroTime }) => {
 	const pomodoroArray = [];
 	let currentDate = set(new Date(), {
-		hours: 13,
-		minutes: 20,
+		hours: 10,
+		minutes: 0,
 		seconds: 0,
 	});
 
